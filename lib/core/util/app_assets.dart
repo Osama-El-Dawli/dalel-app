72dcaf3b0b2be66133cc3f131f9771c54b67447e
Osama-El-Dawli <saidosama283@gmail.com>
2025-02-25 13:28:20 +0200

    add assets, strings, colors and routes files

---
+++ lib/core/util/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgetPassword
+  /// assets/images/forget_password.png
+  static const String assetsImagesForgetPassword = "assets/images/forget_password.png";
+
+  /// Assets for assetsImagesOnBoarding1
+  /// assets/images/on_boarding1.png
+  static const String assetsImagesOnBoarding1 = "assets/images/on_boarding1.png";
+
+  /// Assets for assetsImagesOnBoarding2
+  /// assets/images/on_boarding2.png
+  static const String assetsImagesOnBoarding2 = "assets/images/on_boarding2.png";
+
+  /// Assets for assetsImagesOnBoarding3
+  /// assets/images/on_boarding3.png
+  static const String assetsImagesOnBoarding3 = "assets/images/on_boarding3.png";
+
+  /// Assets for assetsImagesSucccesOrderIcon
+  /// assets/images/succces_order_icon.png
+  static const String assetsImagesSucccesOrderIcon = "assets/images/succces_order_icon.png";
+
+  /// Assets for assetsImagesVector1
+  /// assets/images/vector1.svg
+  static const String assetsImagesVector1 = "assets/images/vector1.svg";
+
+  /// Assets for assetsImagesVector2
+  /// assets/images/vector2.svg
+  static const String assetsImagesVector2 = "assets/images/vector2.svg";
+}
+
---
+++ lib/core/util/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xFFC49E85);
+  static const Color offWhite = Color(0xFFF8F4F9);
+  static const Color deepBrown = Color(0xFF6B4B3E);
+  static const Color lightBrown = Color(0xFF6F6460);
+  static const Color grey = Color(0xffC4C4C4);
+  static const Color lightGrey = Color(0xffB4B4B4);
+  static const Color black = Color(0xFF333333);
+}
